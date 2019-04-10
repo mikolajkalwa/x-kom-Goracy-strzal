c1b1e52b9b875b6d5852e5e5bfd71b8afdcfef6c
Mikolaj Kalwa <mikolajkalwa@gmail.com>
2019-04-10 22:44:55 +0200

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,6 @@
 module.exports = {
     "extends": "airbnb-base",
     "rules": {
-        "indent": ["error", 4],
-        "linebreak-style": ["error", "windows"],
         "no-console": "off",
     },
     "globals": {
--- app.js
+++ app.js
@@ -4,96 +4,95 @@ const axios = require('axios');
 const { webhookURL } = require('./config.json');
 
 function przygotujWiadomosc(produkt) {
-    let wiadomoscDoWyslania = '';
+  let wiadomoscDoWyslania = '';
 
-    Object.entries(produkt).forEach(([key, value]) => {
-        switch (key) {
-        case 'nazwa':
-            wiadomoscDoWyslania += `**${value}**\n`;
-            break;
-        case 'staraCena':
-            wiadomoscDoWyslania += `Stara cena: ${value}\n`;
-            break;
-        case 'nowaCena':
-            wiadomoscDoWyslania += `Nowa cena: ${value}\n`;
-            break;
-        case 'oszczedz':
-            wiadomoscDoWyslania += `Oszczędzasz: ${value}\n`;
-            break;
-        case 'pozostalo':
-            wiadomoscDoWyslania += `Pozostało ${value} sztuk.\n`;
-            break;
-        case 'sprzedano':
-            wiadomoscDoWyslania += `Sprzedano już ${value} sztuk.\n`;
-            break;
-        case 'wyprzedano':
-            wiadomoscDoWyslania += '**Wyprzedano!**\n';
-            break;
-        default:
-            wiadomoscDoWyslania += '';
-        }
-    });
+  Object.entries(produkt).forEach(([key, value]) => {
+    switch (key) {
+      case 'nazwa':
+        wiadomoscDoWyslania += `**${value}**\n`;
+        break;
+      case 'staraCena':
+        wiadomoscDoWyslania += `Stara cena: ${value}\n`;
+        break;
+      case 'nowaCena':
+        wiadomoscDoWyslania += `Nowa cena: ${value}\n`;
+        break;
+      case 'oszczedz':
+        wiadomoscDoWyslania += `Oszczędzasz: ${value}\n`;
+        break;
+      case 'pozostalo':
+        wiadomoscDoWyslania += `Pozostało ${value} sztuk.\n`;
+        break;
+      case 'sprzedano':
+        wiadomoscDoWyslania += `Sprzedano już ${value} sztuk.\n`;
+        break;
+      case 'wyprzedano':
+        wiadomoscDoWyslania += '**Wyprzedano!**\n';
+        break;
+      default:
+        wiadomoscDoWyslania += '';
+    }
+  });
 
-    wiadomoscDoWyslania += 'https://www.x-kom.pl/goracy_strzal/';
-    return wiadomoscDoWyslania;
+  wiadomoscDoWyslania += 'https://www.x-kom.pl/goracy_strzal/';
+  return wiadomoscDoWyslania;
 }
 
 function wykonajWebhook(webhook, wiadomosc) {
-    return axios.post(webhook, {
-        content: wiadomosc,
-    });
+  return axios.post(webhook, {
+    content: wiadomosc,
+  });
 }
 
 (async () => {
-    let browser;
-    try {
-        browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto('https://www.x-kom.pl/hot-shots/current/widget');
+  let browser;
+  try {
+    browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.x-kom.pl/hot-shots/current/widget');
 
-        const produkt = await page.evaluate(() => {
-            const przecenionyProdukt = {
-                nazwa: document.querySelector('#hotShot > div:nth-child(2) > div.col-md-12.col-sm-6.product-impression > p'),
-                staraCena: document.querySelector('#hotShot > div:nth-child(2) > div:nth-child(2) > div.clearfix.price > div.old-price'),
-                nowaCena: document.querySelector('#hotShot > div:nth-child(2) > div:nth-child(2) > div.clearfix.price > div.new-price'),
-                oszczedz: document.querySelector('#hotShot > div:nth-child(2) > div:nth-child(2) > div.clearfix.discount.hidden-md.hidden-lg > span'),
-                pozostalo: document.querySelector('#hotShot > div:nth-child(2) > div:nth-child(2) > div.clearfix.count > div.pull-left > strong'),
-                sprzedano: document.querySelector('#hotShot > div:nth-child(2) > div:nth-child(2) > div.clearfix.count > div.pull-right > strong'),
-                wyprzedano: document.querySelector('#hotShot > div:nth-child(2) > div:nth-child(2) > div.sold-info'),
-            };
+    const produkt = await page.evaluate(() => {
+      const przecenionyProdukt = {
+        nazwa: document.querySelector('#hotShot > div:nth-child(2) > div.col-md-12.col-sm-6.product-impression > p'),
+        staraCena: document.querySelector('#hotShot > div:nth-child(2) > div:nth-child(2) > div.clearfix.price > div.old-price'),
+        nowaCena: document.querySelector('#hotShot > div:nth-child(2) > div:nth-child(2) > div.clearfix.price > div.new-price'),
+        oszczedz: document.querySelector('#hotShot > div:nth-child(2) > div:nth-child(2) > div.clearfix.discount.hidden-md.hidden-lg > span'),
+        pozostalo: document.querySelector('#hotShot > div:nth-child(2) > div:nth-child(2) > div.clearfix.count > div.pull-left > span'),
+        sprzedano: document.querySelector('#hotShot > div:nth-child(2) > div:nth-child(2) > div.clearfix.count > div.pull-right > span'),
+        wyprzedano: document.querySelector('#hotShot > div:nth-child(2) > div:nth-child(2) > div.sold-info'),
+      };
 
-            Object.entries(przecenionyProdukt).forEach(([key, value]) => {
-                if (value === null) {
-                    delete przecenionyProdukt[key];
-                } else {
-                    przecenionyProdukt[key] = value.innerText;
-                }
-            });
-
-            return przecenionyProdukt;
-        });
-
-
-        const wiadomosc = przygotujWiadomosc(produkt);
-
-        if (Array.isArray(webhookURL)) {
-            const wykonywaneWebhooki = [];
-            webhookURL.forEach((webhook) => {
-                wykonywaneWebhooki.push(wykonajWebhook(webhook, wiadomosc));
-            });
-            await Promise.all(wykonywaneWebhooki);
+      Object.entries(przecenionyProdukt).forEach(([key, value]) => {
+        if (value === null) {
+          delete przecenionyProdukt[key];
         } else {
-            await wykonajWebhook(webhookURL, wiadomosc);
+          przecenionyProdukt[key] = value.innerText;
         }
+      });
 
-        await browser.close();
-    } catch (error) {
-        console.error(error);
-    } finally {
-        if (browser) {
-            await browser.close();
-        }
+      return przecenionyProdukt;
+    });
 
-        process.exit(0);
+
+    const wiadomosc = przygotujWiadomosc(produkt);
+
+    if (Array.isArray(webhookURL)) {
+      const wykonywaneWebhooki = [];
+      webhookURL.forEach((webhook) => {
+        wykonywaneWebhooki.push(wykonajWebhook(webhook, wiadomosc));
+      });
+      await Promise.all(wykonywaneWebhooki);
+    } else {
+      await wykonajWebhook(webhookURL, wiadomosc);
     }
+
+    await browser.close();
+  } catch (error) {
+    console.error(error);
+  } finally {
+    if (browser) {
+      await browser.close();
+    }
+    process.exit(0);
+  }
 })();
