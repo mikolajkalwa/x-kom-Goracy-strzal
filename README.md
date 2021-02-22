2e8641004df3a93edeef48a9ae3fe459a66a2ccd
Mikolaj Kalwa <mikolajkalwa@gmail.com>
2021-02-22 15:45:09 +0100

    zmiana logiki, zmiana bibliotek

--- README.md
+++ README.md
@@ -2,12 +2,9 @@ Skrypt pobierający informację co aktualnie znajduje się na gorącym strzale w
 
 #### Instrukcja uruchomienia: 
 * Należy zainstalować [Node.js w wersji LTS](https://nodejs.org/en/).
-* Po instalacji wylogować się i zalogować ponownie.
 * Sklonować to repozytorium na swój dysk.
-* Przejsć do [pliku z konfiguracją](config.json). Należy w nim podać adres URL [webhooka](https://support.discordapp.com/hc/en-us/articles/228383668-Intro-to-Webhooks) lub tablicę zawierającą URL'e webhooków.  
+* Przejsć do [pliku z konfiguracją](config.json). Należy w nim podać adres URL [webhooka](https://support.discordapp.com/hc/en-us/articles/228383668-Intro-to-Webhooks) lub tablicę zawierającą URL'e webhooków.
 * Przejść do folderu z repozytorium i uruchomić komendę `npm install`.
-* Uruchomięc komendę `node app.js`
 
 #### Zalecany scenariusz użycia: 
-* Skrypt zainstalować na komputerze/serwerze z systemem Linux.
-* Skonfigurować cron-a w taki sposób aby uruchamiał on `node app.js` codziennie o godzinie 10 i 22 
+* W przypadku serwera skonfigurować cron-a (albo inny dowolny scheduler) w taki sposób aby uruchamiał on `node app.js` codziennie minutę przed godziną 10 i 22
--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const cheerio = require('cheerio');
-const axios = require('axios');
+const got = require('got');
+const logger = require('pino')();
 
 const { webhookURL } = require('./config.json');
-
-const currentDate = new Date().toISOString();
 
 function convertPriceFormat(price) {
   return `${price},00 zł`;
@@ -59,45 +58,72 @@ function prepareMessage(product) {
 }
 
 function executeWebhook(webhook, message) {
-  return axios.post(webhook, {
-    content: message,
+  return got.post(webhook, {
+    json: {
+      content: message,
+    },
   });
 }
 
-(async () => {
-  try {
-    const website = await axios.get('https://www.x-kom.pl/hot-shots/current/widget');
-    const $ = cheerio.load(website.data);
+async function sendNotification(message) {
+  if (Array.isArray(webhookURL)) {
+    const executingWebhooks = [];
+    webhookURL.forEach((webhook) => {
+      executingWebhooks.push(executeWebhook(webhook, message));
+    });
 
-    const productName = $('.product-name').text();
-    const productId = $('div.col-md-12:nth-child(1)').attr('data-product-id');
-    const oldPrice = $('.old-price').text();
-    const newPrice = $('.new-price').text();
-    const remainingAmount = $('.pull-left .gs-quantity').text();
-    const soldAmount = $('.pull-right .gs-quantity').text();
-    const soldOut = $('.sold-info').text();
-
-    const staraCenaJakoLiczba = parseInt(oldPrice.replace(/ /g, '').slice(0, -5), 10);
-    const nowaCenaJakoLiczba = parseInt(newPrice.replace(/ /g, '').slice(0, -5), 10);
-    const saving = convertPriceFormat(staraCenaJakoLiczba - nowaCenaJakoLiczba);
-
-    const product = {
-      productName, oldPrice, newPrice, saving, remainingAmount, soldAmount, soldOut, productId,
-    };
-
-    const message = prepareMessage(deleteEmptyProperties(product));
-
-    if (Array.isArray(webhookURL)) {
-      const executingWebhooks = [];
-      webhookURL.forEach((webhook) => {
-        executingWebhooks.push(executeWebhook(webhook, message));
-      });
-
-      await Promise.all(executingWebhooks);
-    } else {
-      await executeWebhook(webhookURL, message);
-    }
-  } catch (error) {
-    console.error(currentDate, error);
+    await Promise.all(executingWebhooks);
+  } else {
+    await executeWebhook(webhookURL, message);
   }
+}
+
+async function getProduct() {
+  const website = await got('https://www.x-kom.pl/hot-shots/current/widget');
+  const $ = cheerio.load(website.body);
+
+  const productName = $('.product-name').text();
+  const productId = $('div.col-md-12:nth-child(1)').attr('data-product-id');
+  const oldPrice = $('.old-price').text();
+  const newPrice = $('.new-price').text();
+  const remainingAmount = $('.pull-left .gs-quantity').text();
+  const soldAmount = $('.pull-right .gs-quantity').text();
+  const soldOut = $('.sold-info').text();
+
+  const oldPriceAsNumber = parseInt(oldPrice.replace(/ /g, '').slice(0, -5), 10);
+  const newPriceAsNumber = parseInt(newPrice.replace(/ /g, '').slice(0, -5), 10);
+  const saving = convertPriceFormat(oldPriceAsNumber - newPriceAsNumber);
+
+  const product = {
+    productName, oldPrice, newPrice, saving, remainingAmount, soldAmount, soldOut, productId,
+  };
+  return product;
+}
+
+async function notifyAboutNewPromotion(oldPromotion, retries = 1) {
+  const newPromotion = await getProduct();
+  if (newPromotion.productId !== oldPromotion.productId) {
+    logger.info('Fetched products are different. Finish recurssion!');
+    const message = prepareMessage(deleteEmptyProperties(newPromotion));
+    await sendNotification(message);
+    process.exit(0);
+  }
+
+  logger.info(`Product haven't changed. Attempt: ${retries}`);
+  setTimeout(async () => notifyAboutNewPromotion(oldPromotion, retries + 1), retries * 5 * 1000);
+}
+
+(async () => {
+  const oldPromotion = await getProduct();
+
+  setTimeout(async () => {
+    await notifyAboutNewPromotion(oldPromotion);
+  }, 90 * 1000);
+  // assuming the script runs at 9:59, we give x-kom additional 30 seconds to update hot shot
+
+  // terminate the process after 8 minutes:
+  setTimeout(() => {
+    logger.error('Unable to fetch new promotion.');
+    process.exit(1);
+  }, 8 * 60 * 1000);
 })();
